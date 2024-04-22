7089ccb69d27c514e31f446666313d8c5364d60a
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-04-22 14:08:42 +0530

    Update Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,7 +1,8 @@
-module hs(a,b,difference,borrow);
+module halfsubractor(a,b,diff,borrow);
 input a,b;
-output difference,borrow;
-
-
-
+output diff,borrow;
+wire w1;
+xor g1(diff,a,b);
+not g2(w1,a);
+and g3(borrow,w1,b);
 endmodule
